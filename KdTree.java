45dff29fa93a516449abb23a2df5b54cd1241f78
Ottriman <gogo.gogo520@gmail.com>
2016-09-29 18:28:09 +0000

    Contains og Insert
    Contains og Insert virka núna rétt og gefa rétt scar hjá mooshak. Ég
    breytti main fallinu yfir í það sem A hlutinn í KDtree hultanum af
    mooshak hérna notaði.
    
    Getur breytt því í það sem B eða C hlutinn notar þegar þú gerir það :)

--- KdTree.java
+++ KdTree.java
@@ -11,7 +11,8 @@ import edu.princeton.cs.algs4.*;
 
 public class KdTree {
 
-    private Node root; // root of the tree
+    private int size = 0; // Grimur: size of tree value
+	private Node root; // root of the tree
     private static final boolean HORIZONTAL = true;
     private static final boolean VERTICAL = false;
 
@@ -20,14 +21,14 @@ public class KdTree {
         private Point2D p; // the point
         private RectHV rect; // the axis-aligned rectangle corresponding to this node
         private Node left, right; // the left and right/bottom subtree
-        private int level; // the tree level
+        private boolean vertical; // the tree level
 
-        public Node(Point2D p, Node left, Node right, RectHV rect, int level) {
+        public Node(Point2D p, Node left, Node right, RectHV rect, boolean vertical) {
             this.p = p;
             this.rect = rect;
             this.left = left;
             this.right = right;
-            this.level = level;
+            this.vertical = vertical;
 
         }
     }
@@ -51,21 +52,34 @@ public class KdTree {
     }
 
     // return number of key-value pairs in BST rooted at x
-    private int size(Node node) {
-        if (node == null)
-            return 0;
-        else
-            return size(node);
+    // Grimur: See nothing about needing that as opposed to just for the tree as test main function shows?
+    private int size(Node node) 
+    {
+        int returnSize = size;
+        
+        return returnSize;
     }
 
     // add the point p to the set (if it is not already in the set)
     public void insert(Point2D p) {
-        root = insertHelper(root, p, HORIZONTAL);
+        root = insertHelper(root, p, true);
     }
 
-    private Node insertHelper(Node node, Point2D point, boolean vertical){
-        if (node == null) {
-            return null;
+    private Node insertHelper(Node node, Point2D point, boolean vertical)
+    {
+        if (node == null) 
+        {
+            RectHV newRect = new RectHV(0.0, 0.0, 1.0, 1.0);
+        	
+        	Node newNode = new Node(point, null, null, newRect, !vertical);
+        	size++;
+        	
+        	return newNode;
+        }
+        
+        if (node.p.x() == point.x() && node.p.y() == point.y())
+        {
+        	return node;
         }
 
         /**
@@ -78,34 +92,90 @@ public class KdTree {
          * if the point to be inserted has a smaller y-coordinate than the point
          * in the NODE, got to left else got to right
          */
+        
+        // Grimur: Need to compare y values when vertical is true
 
-        if(node.p.compareTo(point) > 0){
-            node.left = insertHelper(node.left, point, !vertical);
-        }else if(node.p.compareTo(point) < 0){
-            node.right = insertHelper(node.right, point, !vertical);
-        }else{
-            node.p = point;
+        if(vertical)
+        {
+            if(node.p.x() > point.x())
+            {
+            	node.right = insertHelper(node.right, point, !vertical);
+            }
+            else
+            {
+            	node.left = insertHelper(node.left, point, !vertical);
+            } 
+        }
+        
+        else if(!vertical)
+        {
+            if(node.p.y() > point.y())
+            {
+            	node.right = insertHelper(node.right, point, vertical);
+            }
+            else
+            {
+            	node.left = insertHelper(node.left, point, vertical);
+            } 
         }
         return node;
     }
 
     // does the set contain the point p?
-    public boolean contains(Point2D p) {
-
-        return false;
+    //Grimur: Made it void and added helper, making helper determine the outcome
+    public boolean contains(Point2D p) 
+    { 	
+    	return containsHelper(root, p, true);
     }
 
     /**
      * TODO: CREATE PRIVATE HELPER FOR contains()
      */
+    
+    private boolean containsHelper (Node node, Point2D point, boolean vertical)
+    {
+    	if (node == null)
+    	{
+    		return false;
+    	}
+    	
+    	if(node.p.x() == point.x() && node.p.y() == point.y())
+    	{
+    		return true;
+    	}
+    	
+    	if(vertical)
+        {
+            if(node.p.x() > point.x())
+            {
+            	return containsHelper(node.right, point, !vertical);
+            }
+            else
+            {
+            	 return containsHelper(node.left, point, !vertical);
+            } 
+        }
+        else
+        {
+            if(node.p.y() > point.y())
+            {
+            	return containsHelper(node.right, point, vertical);
+            }
+            else
+            {
+            	return containsHelper(node.left, point, vertical);
+            } 
+        }
+    }
 
     // draw all of the points to standard draw
-    public void draw() {
-
+    public void draw() 
+    {
         drawHelper(root, null);
     }
 
-    private void drawHelper(Node node, Node parent) {
+    private void drawHelper(Node node, Node parent) 
+    {
         if (node == null) return;
 
         StdDraw.setPenColor(StdDraw.BLACK);
@@ -182,55 +252,24 @@ public class KdTree {
     public static void main(String[] args) {
         In in = new In();
         Out out = new Out();
-        int nrOfRecangles = in.readInt();
-        int nrOfPointsCont = in.readInt();
-        int nrOfPointsNear = in.readInt();
-        RectHV[] rectangles = new RectHV[nrOfRecangles];
-        Point2D[] pointsCont = new Point2D[nrOfPointsCont];
-        Point2D[] pointsNear = new Point2D[nrOfPointsNear];
-        for (int i = 0; i < nrOfRecangles; i++) {
-            rectangles[i] = new RectHV(in.readDouble(), in.readDouble(),
-                    in.readDouble(), in.readDouble());
+        int N = in.readInt(), C = in.readInt(), T = 20;
+        KdTree tree = new KdTree();
+        Point2D [] points = new Point2D[C];
+        out.printf("Inserting %d points into tree\n", N);
+        for (int i = 0; i < N; i++) {
+            tree.insert(new Point2D(in.readDouble(), in.readDouble()));
         }
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            pointsCont[i] = new Point2D(in.readDouble(), in.readDouble());
+        out.printf("tree.size(): %d\n", tree.size());
+        out.printf("Testing contains method, querying %d points\n", C);
+        for (int i = 0; i < C; i++) {
+            points[i] = new Point2D(in.readDouble(), in.readDouble());
+            out.printf("%s: %s\n", points[i], tree.contains(points[i]));
         }
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            pointsNear[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        KdTree set = new KdTree();
-        for (int i = 0; !in.isEmpty(); i++) {
-            double x = in.readDouble(), y = in.readDouble();
-            set.insert(new Point2D(x, y));
-        }
-        for (int i = 0; i < nrOfRecangles; i++) {
-            // Query on rectangle i, sort the result, and print
-            Iterable<Point2D> ptset = set.range(rectangles[i]);
-            int ptcount = 0;
-            for (Point2D p : ptset)
-                ptcount++;
-            Point2D[] ptarr = new Point2D[ptcount];
-            int j = 0;
-            for (Point2D p : ptset) {
-                ptarr[j] = p;
-                j++;
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < C; j++) {
+                tree.contains(points[j]);
             }
-            Arrays.sort(ptarr);
-            out.println("Inside rectangle " + (i + 1) + ":");
-            for (j = 0; j < ptcount; j++)
-                out.println(ptarr[j]);
         }
-        out.println("Contain test:");
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            out.println((i + 1) + ": " + set.contains(pointsCont[i]));
-        }
-
-        out.println("Nearest test:");
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            out.println((i + 1) + ": " + set.nearest(pointsNear[i]));
-        }
-
-        out.println();
     }
 }
 
